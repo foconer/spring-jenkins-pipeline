37fd19e41858260ddd18438766aafc0afd3599a2
Frederick Oconer <oconer@Fredericks-MacBook-Pro.local>
2018-09-30 01:44:02 -0600

    removed classes

--- app-ws/app-restws-integration/src/test/java/hello/HelloControllerIntegrationTest.java
+++ app-ws/app-restws-integration/src/test/java/hello/HelloControllerIntegrationTest.java
@@ -1,42 +1,25 @@
 package hello;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import javax.servlet.ServletContext;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { Application.class })
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { hello.Application.class }, webEnvironment = RANDOM_PORT)
+@AutoConfigureWebTestClient
 public class HelloControllerIntegrationTest {
 
     @Autowired
-    private WebApplicationContext wac;
+    private WebTestClient webClient;
 
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    @Test
+    public void testHelloAPI() throws Exception {
+        this.webClient.get().uri("hello").exchange().expectStatus().isOk();
     }
-
-//    @Test
-//    public void givenWac_whenServletContext_thenItProvidesGreetController() {
-//        ServletContext servletContext = wac.getServletContext();
-//
-//        Assert.assertNotNull(servletContext);
-//        Assert.assertTrue(servletContext instanceof MockServletContext);
-//        Assert.assertNotNull(wac.getBean("helloController"));
-//    }
 }
